cf2f2de1f8e002e585e4478425b41ecd0cc7b223
sonali-dudekula <sonalidudekula@gmail.com>
2022-07-11 10:08:38 +0530

    themes, functions, routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import "package:flutter/material.dart";
-import 'package:flutter_sample_project_1/homepage.dart';
+import 'package:flutter_sample_project_1/pages/homepage.dart';
 
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(), only to be included if default route is not specified in routes section
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.orange),
+      darkTheme: ThemeData(
+        // primarySwatch: Colors.orange, has no effect because brightness property overrides this
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => HomePage(),
+      },
+
     );
     
   }
